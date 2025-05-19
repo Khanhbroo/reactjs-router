b68ca0f276801c7b368681d0b7050a406a907f35
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-19 18:46:14 +0700

    Navigating between Pages with Links

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const ProductsPage = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <p>
+        Go to <Link to="/">home</Link>
+      </p>
+    </>
+  );
 };
 
 export default ProductsPage;
