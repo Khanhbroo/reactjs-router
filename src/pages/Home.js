0fc60560e6777a4e3be05d433984e3d3129e2470
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-21 14:11:50 +0700

    Navigating Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>My Home Page</h1>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 };
